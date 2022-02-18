6562f234bba06bc1b9619cc3d7740f62c91905b8
Angelique Priam <angelique.priam@hotmail.com>
2022-02-18 02:00:47 +0100

    Integration HTML ended

--- front/js/product.js
+++ front/js/product.js
@@ -3,12 +3,11 @@ main()
 async function main() {
     const productId = getProductId()
     const product = await getProducts(productId)
-    console.log(product)
     displayProducts(product)
+    createBasket(productId)
 }
-
 function getProductId () {
-    return new URL(location.href).searchParams.get('id')
+    return new URL(document.location).searchParams.get('id')
 }
 function getProducts(productId) {
     return fetch(`http://localhost:3000/api/products/${productId}`)
@@ -22,11 +21,25 @@ function getProducts(productId) {
         console.log(error);
     })
     }
-    
     function displayProducts(product) {
-        document.getElementsByClassName("item__img").innerHTML += `<img src="${product.imageUrl}" alt="${product.altTxt}">`
-        document.getElementById("title").innerHTML += `${product.name}`
-        document.getElementById("price").innerHTML += `${product.price}`
-        document.getElementById("description").innerHTML += `${product.description}`
-        document.getElementById("colors").innerHTML += `<option value="${product.colors}">${product.colors}</option>`
+        document.querySelector('.item__img').innerHTML = (`<img src="${product.imageUrl}" alt="${product.altTxt}">`)
+        document.getElementById("title").innerHTML = `${product.name}`
+        document.getElementById("price").innerHTML = `${product.price}`
+        document.getElementById("description").innerHTML = `${product.description}`
+        for(let color of product.colors){
+        document.getElementById("colors").innerHTML += `<option value="${color}">${color}</option>`
+        };
     }
+
+    function createBasket (productId) {
+        document.getElementById('addToCart').addEventListener('click', () => {
+        let color = (document.getElementById("colors").value)
+        let quantity = (document.getElementById("quantity").value)
+        let id = productId
+        let basket = {
+            color,
+            quantity,
+            id
+        }
+        
+    })}
